9153a5ed7ab5f27f45d187825f7587a15a7b004e
mfonken <mgfonken@gmail.com>
2019-08-17 14:00:20 -0600

    Removed extra files

--- deco/rho_deco/utilities/algorithm/ring_buffer.c
+++
@@ -1,9 +0,0 @@
-//
-//  ring_buffer.c
-//  pcrxprm
-//
-//  Created by Matthew Fonken on 2/5/19.
-//  Copyright © 2019 Matthew Fonken. All rights reserved.
-//
-
-#include "ring_buffer.h"
--- pcrxprm/v3/pcrxprm/master/platforms/platform_interface_master.h
+++
@@ -1,8 +0,0 @@
-#ifndef platform_interface_master_h
-#define platform_interface_master_h
-
-#ifdef __STM32__
-#include "stm32_interface.h"
-#endif
-
-#endif /* platform_interface_master_h */
